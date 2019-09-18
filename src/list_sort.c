87b628c8d45396b60a3b39abc852b0342973ce59
Kharazim Sharlene <ksharlen@mi-u3.21-school.ru>
2019-09-18 12:29:25 +0300

    added func "find_end_repeat"

--- src/list_sort.c
+++ src/list_sort.c
@@ -6,7 +6,7 @@
 /*   By: ksharlen <ksharlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 09:48:23 by ksharlen          #+#    #+#             */
-/*   Updated: 2019/09/18 07:02:48 by ksharlen         ###   ########.fr       */
+/*   Updated: 2019/09/18 12:28:36 by ksharlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,15 +33,24 @@ static t_filename	*merge_sort(t_filename *list,
 		merge_sort(right, sort_key, NULL), sort_key));
 }
 
-static t_filename	*skip_sort(t_filename *beg, size_t *count)
+static t_filename	*find_end_repeat(t_filename *beg, int (*cmp)(t_filename *, t_filename *))
 {
-	
+	if (beg)
+	{
+		while (beg->next && !cmp(beg, beg->next))
+		{
+			ft_printf("filename: %s\n", beg->filename);
+			beg = beg->next;
+		}
+	}
+	return (beg);
 }
 
 static t_filename	*sort_repeat(t_filename *beg, int (*sort_key)(t_filename *, t_filename *))
 {
 	t_filename *begin_list;
-	t_filename *beg_repeat;
+	t_filename *end;
+	t_filename *tmp;
 
 	begin_list = NULL;
 	if (beg)
@@ -51,20 +60,26 @@ static t_filename	*sort_repeat(t_filename *beg, int (*sort_key)(t_filename *, t_
 		{
 			if (!sort_key(beg, beg->next))
 			{
-				if (beg == begin_list)
-				{
-					begin_list = sort_repeat();
-					beg = begin_list;
-				}
-				else
-					beg = sort_repeat();
-				beg = skip_sort();
+				end = find_end_repeat(beg, sort_key);
+				ft_printf("end: %s\n", end->filename);
+				tmp = end->next;
+				beg = end;
 			}
 			else
 				beg = beg->next;
 		}
 	}
+	return (NULL); //!временно
 }
+
+//?		1)	Найти начало повторяющегося интервала
+//?		2)	Найти последний повторяющийся элемент
+//?		3)	Сохранить элемент после последнего(что бы не потерять связь)
+//?		4)	занулить конечный повторный элемент
+//?		5)	отсортировать повторки через merge_sort
+//?		6)	Найти последний элемент в отсортированном списке
+//?		7)	Присвоить ему сохраненную связь
+//?		8)	Перенести указатель на конец репитов
 
 t_filename			*sort_list_by_flags(t_filename **beg, t_ubyte *flags)
 {
@@ -80,7 +95,8 @@ t_filename			*sort_list_by_flags(t_filename **beg, t_ubyte *flags)
 			else
 			{
 				(*beg) = merge_sort(*beg, cmp_mtime, NULL);
-				//(*beg) = sort_repeat(*beg, cmp_mtime);
+				(*beg) = sort_repeat(*beg, cmp_mtime);
+				//exit(EXIT_SUCCESS);
 			}
 		}
 		else
